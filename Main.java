33ed01a715e5134c0adf0474a69c0b9ee01f9182
Spyder <usubhadeep123@gmail.com>
2026-05-24 15:27:02 +0530

    added sample file

---
+++ Main.java
@@ -0,0 +1,62 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) {
+        // Using Fast Scanner to handle large inputs efficiently
+        FastScanner sc = new FastScanner();
+        solve(sc);
+    }
+
+    private static void solve(FastScanner sc) {
+        // YOUR CODE LIES HERE...
+    }
+
+    // Fast I/O Utility Class
+    static class FastScanner {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer("");
+
+        String next() {
+            while (!st.hasMoreTokens()) {
+                try {
+                    String line = br.readLine();
+                    if (line == null)
+                        return null;
+                    st = new StringTokenizer(line);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        boolean hasNext() {
+            while (!st.hasMoreTokens()) {
+                try {
+                    String line = br.readLine();
+                    if (line == null)
+                        return false;
+                    st = new StringTokenizer(line);
+                } catch (IOException e) {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+    }
+}
