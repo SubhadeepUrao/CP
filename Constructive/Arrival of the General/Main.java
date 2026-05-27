dbeb157dd1a3b4d5382ed06aa9360054e5876916
Spyder <usubhadeep123@gmail.com>
2026-05-27 11:43:02 +0530

    added arrival of general

---
+++ Constructive/Arrival of the General/Main.java
@@ -0,0 +1,74 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) {
+        // Using Fast Scanner to handle large inputs efficiently
+        FastScanner sc = new FastScanner();
+        solve(sc);
+    }
+
+    private static void solve(FastScanner sc) {
+        int N = sc.nextInt();
+        int[] nums = new int[N];
+        int maxPos = 0, minPos = 0;
+        for (int i = 0; i < N; ++i) {
+            nums[i] = sc.nextInt();
+            if (nums[minPos] >= nums[i]) minPos = i;
+            if (nums[maxPos] < nums[i]) maxPos = i;
+        }
+
+        int mini = N - minPos - 1;
+        int maxi = minPos < maxPos ? maxPos - 1 : maxPos;
+
+        System.out.println(mini + maxi);
+    }
+
+    // Fast I/O Utility Class
+    static class FastScanner {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer("");
+
+        String next() {
+            while (!st.hasMoreTokens()) {
+                try {
+                    String line = br.readLine();
+                    if (line == null)
+                        return null;
+                    st = new StringTokenizer(line);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        boolean hasNext() {
+            while (!st.hasMoreTokens()) {
+                try {
+                    String line = br.readLine();
+                    if (line == null)
+                        return false;
+                    st = new StringTokenizer(line);
+                } catch (IOException e) {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+    }
+}
