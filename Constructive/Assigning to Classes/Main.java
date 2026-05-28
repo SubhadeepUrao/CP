979681ad94fb6e5ac7f10e79b9810793e02b37d0
Spyder <usubhadeep123@gmail.com>
2026-05-28 19:07:46 +0530

    updated assigning to classes

--- Constructive/Assigning to Classes/Main.java
+++ Constructive/Assigning to Classes/Main.java
@@ -5,12 +5,6 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-    // Using Fast Scanner to handle large inputs efficiently
-    FastScanner sc = new FastScanner();
-    solve(sc);
-    }
-
     private static void solve(FastScanner sc) {
         int t = sc.nextInt();
         while (t-- > 0) {
@@ -23,52 +17,6 @@ public class Main {
             Arrays.sort(nums);
 
             System.out.println(nums[n] - nums[n - 1]);
-        }
-    }
-
-    // Fast I/O Utility Class
-    static class FastScanner {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer("");
-
-        String next() {
-            while (!st.hasMoreTokens()) {
-                try {
-                    String line = br.readLine();
-                    if (line == null)
-                        return null;
-                    st = new StringTokenizer(line);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-            return st.nextToken();
-        }
-
-        boolean hasNext() {
-            while (!st.hasMoreTokens()) {
-                try {
-                    String line = br.readLine();
-                    if (line == null)
-                        return false;
-                    st = new StringTokenizer(line);
-                } catch (IOException e) {
-                    return false;
-                }
-            }
-            return true;
-        }
-
-        int nextInt() {
-            return Integer.parseInt(next());
-        }
-
-        long nextLong() {
-            return Long.parseLong(next());
-        }
-
-        double nextDouble() {
-            return Double.parseDouble(next());
         }
     }
 }
