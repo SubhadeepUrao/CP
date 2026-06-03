170fedd16a2d22df9b447371ac478da256f70b4e
Spyder <usubhadeep123@gmail.com>
2026-06-03 16:17:13 +0530

    added ichihime and triangle

---
+++ Maths/Ichihime and Triangle/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    private static void solve(FastScanner sc) {
+        int t = sc.nextInt();
+        while (t-- > 0) {
+
+            // sum of any two sides > other side
+
+            // int a = sc.nextInt();
+            // int b = sc.nextInt();
+            // int c = sc.nextInt();
+            // int d = sc.nextInt();
+
+            // int x = b;
+            // int y = c;
+            // int z = c;
+
+            sc.nextInt();
+            int x = sc.nextInt();
+            int y, z = y = sc.nextInt();
+            sc.nextInt();
+
+            System.out.println(x + " " + y + " " + z);
+        }
+    }
+}
