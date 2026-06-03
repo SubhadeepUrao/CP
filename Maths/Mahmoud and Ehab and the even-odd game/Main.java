e3784ca2d414f790d90f41575593a4627c7fe15d
Spyder <usubhadeep123@gmail.com>
2026-06-03 17:53:48 +0530

    added even odd game

---
+++ Maths/Mahmoud and Ehab and the even-odd game/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    private static void solve(FastScanner sc) {
+        int n = sc.nextInt();
+
+        if ((n & 1) == 0)
+            System.out.println("Mahmoud");
+        else
+            System.out.println("Ehab");
+    }
+}
