cc53c55927cdf6f790f8271b8aca98eaf1a02d81
Spyder <usubhadeep123@gmail.com>
2026-06-03 23:40:20 +0530

    added park lighting

---
+++ Maths/Park Lighting/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    private static void solve(FastScanner sc) {
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            int m = sc.nextInt();
+            System.out.println((n * m >> 1) + (n * m % 2));
+        }
+    }
+}
