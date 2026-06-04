fa77903314c607fa0f0974d1e2ca8a51976fe3d5
Spyder <usubhadeep123@gmail.com>
2026-06-04 15:54:01 +0530

    added magical sticks

---
+++ Maths/Magical Sticks/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    private static void solve(FastScanner sc) {
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int N = sc.nextInt();
+            System.out.println((N + 1) >> 1);
+        }
+    }
+}
