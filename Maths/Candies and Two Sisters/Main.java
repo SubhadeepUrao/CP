358347da23ac29018825603cd8c035b33523d238
Spyder <usubhadeep123@gmail.com>
2026-06-05 15:49:27 +0530

    added candies and two sisters

---
+++ Maths/Candies and Two Sisters/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    private static void solve(FastScanner sc) {
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int n = sc.nextInt();
+
+            System.out.println(n - n / 2 - 1);
+        }
+    }
+}
