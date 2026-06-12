56970b967e453634d593a95d6c51d575d8ba94fa
Spyder <usubhadeep123@gmail.com>
2026-06-13 00:11:58 +0530

    added pipeline

---
+++ Binary Search/Pipeline/Main.java
@@ -0,0 +1,44 @@
+public class Main {
+    private static long n = 0, k = 0;
+
+    private static boolean isValid(long x) {
+        // x < k is already valid so no need to check
+        if (x < 1)
+            return false;
+
+        long maxsum = x * (2 * k - x + 1) / 2;
+        long target = n + x - 1;
+        return target <= maxsum;
+    }
+
+    private static void solve(FastScanner sc) {
+        n = sc.nextLong();
+        k = sc.nextLong();
+
+        if (n == 1) {
+            System.out.println(0);
+            return;
+        }
+        // If even the maximum capacity is not enough, it's impossible.
+        // long maxPossibleSplitters = k - 1;
+        // long maxPossibleSum = maxPossibleSplitters * (2 * k - maxPossibleSplitters + 1) / 2;
+        // long absoluteMaxPipes = maxPossibleSum - maxPossibleSplitters + 1;
+
+        // if (absoluteMaxPipes < n) {
+        //     System.out.println(-1);
+        //     return;
+        // }
+
+        long low = 1;
+        long high = k - 1;
+
+        while (low <= high) {
+            long mid = (low + high) >>> 1;
+            if (isValid(mid))
+                high = mid - 1;
+            else
+                low = mid + 1;
+        }
+        System.out.println(low < k ? low : -1);
+    }
+}
---
+++ Binary Search/Pipeline/MainII.java
@@ -0,0 +1,55 @@
+public class Main {
+    private static long totalNeeded;
+    private static long k;
+
+    // Checks if taking 'mid' number of the largest splitters is enough
+    private static boolean isValid(long mid) {
+        // Sum of all splitters from 1 to k-1
+        long totalSum = (k - 1) * k / 2;
+
+        // Sum of the remaining smaller splitters that we DO NOT take
+        long remainingCount = (k - 1) - mid;
+        long remainingSum = remainingCount * (remainingCount + 1) / 2;
+
+        // The net gain from taking the 'mid' largest splitters
+        long currentGain = totalSum - remainingSum;
+
+        return currentGain >= totalNeeded;
+    }
+
+    private static void solve(FastScanner sc) {
+        long n = sc.nextLong();
+        k = sc.nextLong();
+
+        // If we already have enough or need no splitters
+        if (n == 1) {
+            System.out.println(0);
+            return;
+        }
+
+        totalNeeded = n - 1;
+        long maxPossibleGain = (k - 1) * k / 2;
+
+        // If even using all splitters isn't enough
+        if (maxPossibleGain < totalNeeded) {
+            System.out.println(-1);
+            return;
+        }
+
+        long low = 1;
+        long high = k - 1;
+        long ans = high;
+
+        while (low <= high) {
+            long mid = (low + high) >>> 1;
+            if (isValid(mid)) {
+                ans = mid; // This number of splitters works, try to find a smaller valid amount
+                high = mid - 1;
+            } else {
+                low = mid + 1; // Not enough pipes, we need more splitters
+            }
+        }
+
+        System.out.println(ans);
+    }
+}
