31e4d681d0a1d1d46a372df6478ea71c578e5a72
Spyder <usubhadeep123@gmail.com>
2026-07-29 20:19:02 +0530

    updated dynamic diameter

--- Trees/Dynamic Diameter/Main.java
+++ Trees/Dynamic Diameter/Main.java
@@ -1,9 +1,3 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
-
 public class Main {
     public static void main(String[] args) {
         // Using Fast Scanner to handle large inputs efficiently
@@ -100,51 +94,5 @@ public class Main {
             str.append(res[i]).append('\n');
 
         System.out.println(str);
-    }
-
-    // Fast I/O Utility Class
-    static class FastScanner {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer("");
-
-        String next() {
-            while (!st.hasMoreTokens()) {
-                try {
-                    String line = br.readLine();
-                    if (line == null)
-                        return null;
-                    st = new StringTokenizer(line);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-            return st.nextToken();
-        }
-
-        boolean hasNext() {
-            while (!st.hasMoreTokens()) {
-                try {
-                    String line = br.readLine();
-                    if (line == null)
-                        return false;
-                    st = new StringTokenizer(line);
-                } catch (IOException e) {
-                    return false;
-                }
-            }
-            return true;
-        }
-
-        int nextInt() {
-            return Integer.parseInt(next());
-        }
-
-        long nextLong() {
-            return Long.parseLong(next());
-        }
-
-        double nextDouble() {
-            return Double.parseDouble(next());
-        }
     }
 }
