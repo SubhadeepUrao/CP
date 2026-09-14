723f1acfbb3f28a16308ad407cca05372ad2c475
Spyder <usubhadeep123@gmail.com>
2026-09-14 20:30:39 +0530

    optimal to Main I solution + no TLE

--- Trie/Polycarp's phone book/Main II.java
+++ Trie/Polycarp's phone book/Main II.java
@@ -1,18 +1,4 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
-import java.util.StringTokenizer;
-
 public class Main {
-    public static void main(String[] args) {
-        // Using Fast Scanner to handle large inputs efficiently
-        FastScanner sc = new FastScanner();
-        solve(sc);
-    }
 
     private static void solve(FastScanner sc) {
         int n = sc.nextInt();
@@ -56,51 +42,5 @@ public class Main {
             res.append(best).append('\n');
         }
         System.out.println(res);
-    }
-
-    // Fast I/O Utility Class
-    static class FastScanner {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer("");
-
-        String next() {
-            while (!st.hasMoreTokens()) {
-                try {
-                    String line = br.readLine();
-                    if (line == null)
-                        return null;
-                    st = new StringTokenizer(line);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-            return st.nextToken();
-        }
-
-        boolean hasNext() {
-            while (!st.hasMoreTokens()) {
-                try {
-                    String line = br.readLine();
-                    if (line == null)
-                        return false;
-                    st = new StringTokenizer(line);
-                } catch (IOException e) {
-                    return false;
-                }
-            }
-            return true;
-        }
-
-        int nextInt() {
-            return Integer.parseInt(next());
-        }
-
-        long nextLong() {
-            return Long.parseLong(next());
-        }
-
-        double nextDouble() {
-            return Double.parseDouble(next());
-        }
     }
 }
