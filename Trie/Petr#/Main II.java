afe35d94d5b648ba02385ea9283695a4f060dae0
Spyder <usubhadeep123@gmail.com>
2026-09-17 01:46:50 +0530

    added fast 1D-trie + no TLE

--- Trie/Petr#/Main II.java
+++ Trie/Petr#/Main II.java
@@ -1,14 +1,4 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
 public class Main {
-    public static void main(String[] args) {
-        // Using Fast Scanner to handle large inputs efficiently
-        FastScanner sc = new FastScanner();
-        solve(sc);
-    }
 
     private static int countSubstrings(String T, String prefix, String suffix) {
         int n = T.length();
@@ -63,51 +53,5 @@ public class Main {
         String suffix = sc.next();
 
         System.out.println(countSubstrings(T, prefix, suffix));
-    }
-
-    // Fast I/O Utility Class
-    static class FastScanner {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer("");
-
-        String next() {
-            while (!st.hasMoreTokens()) {
-                try {
-                    String line = br.readLine();
-                    if (line == null)
-                        return null;
-                    st = new StringTokenizer(line);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-            return st.nextToken();
-        }
-
-        boolean hasNext() {
-            while (!st.hasMoreTokens()) {
-                try {
-                    String line = br.readLine();
-                    if (line == null)
-                        return false;
-                    st = new StringTokenizer(line);
-                } catch (IOException e) {
-                    return false;
-                }
-            }
-            return true;
-        }
-
-        int nextInt() {
-            return Integer.parseInt(next());
-        }
-
-        long nextLong() {
-            return Long.parseLong(next());
-        }
-
-        double nextDouble() {
-            return Double.parseDouble(next());
-        }
     }
 }
